c26e99ca53b53638a45489130a040daef44ba8c5
hachi1994 <lhc1092751647@163.com>
2025-01-09 18:00:46 +0800

    #20241202# 提交

---
+++ 算法/array_1.js
@@ -0,0 +1,38 @@
+// 合并2个有序数组
+let foo = function (arr1 = [], n1, arr2 = [], n2) {
+    let i = n1 - 1, j = n2 - 1, h = n1 + n2 - 1;
+    while (i >= 0 && j >= 0) {
+        if (arr1[i] > arr2[j]) {
+            arr1[h] = arr1[i];
+            i--;
+            h--;
+        } else {
+            arr1[h] = arr2[j];
+            j--;
+            h--;
+        }
+    }
+    while (j > 0) {
+        arr1[h] = arr2[j];
+        j--;
+        h--;
+    }
+    return arr1;
+}
+// 三数求和
+let foo2 = function (arr = [-1, 0, 1, 2, -1, -4]) {
+    let res = [];
+    let nums = arr.sort((a, b) => a - b);
+    let lens = nums.length;
+    for (let i = 0; i < lens - 2; i++) {
+        let j = i + 1;
+        let k = lens - 1;
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+        while (j < k) {
+
+        }
+    }
+
+}
